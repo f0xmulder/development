dc9db528a86ca38a94d50ecbb6bbebde357298e1
Niels Dequeker <niels@newfront.eu>
2019-02-18 12:20:06 +0100

    Extend App tests + configure Enzyme

--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import { shallow } from 'enzyme'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it('contains the page title', () => {
+  const wrapper = shallow(<App/>)
+  expect(wrapper.find('h1').text()).toBe('API overview')
+})
+
+describe('on initialization', () => {
+  it('should fetch the available apis', () => {
+    jest.spyOn(App.prototype, 'fetchApiList')
+
+    const wrapper = shallow(<App/>)
+    expect(wrapper.instance().fetchApiList).toHaveBeenCalled()
+  })
+
+  it('should store the available apis as state', () => {
+    const apiPromise = Promise.resolve([{test: 'test'}])
+    App.prototype.fetchApiList = jest.fn(() => apiPromise)
+
+    const wrapper = shallow(<App/>)
+    return apiPromise
+        .then(() => {
+          expect(wrapper.state('apis')).toEqual([{ test: 'test'}])
+        })
+  })
+})
+
+describe('listing the available apis', () => {
+  let listItems
+
+  beforeEach(() => {
+    const wrapper = shallow(<App/>)
+
+    const apis = [{ organization_name: 'Test' }]
+    wrapper.setState({ apis })
+
+    listItems = wrapper.find('ul li')
+  })
+
+  it('should show all available APIs', () => {
+    expect(listItems.length).toBe(1)
+  })
+
+  it('should show the organization name as label', () => {
+    expect(listItems.first().text()).toBe('Test')
+  })
+})
