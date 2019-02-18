0f425f546e2e71257761991b3b403ffdcd08f4ec
Niels Dequeker <niels@newfront.eu>
2019-02-18 10:29:52 +0100

    Move listen tests to the listen_test file

--- api/api_test.go
+++ api/api_test.go
@@ -4,11 +4,8 @@
 package api
 
 import (
-	"io/ioutil"
-	"net/http/httptest"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
@@ -25,35 +22,4 @@ func TestAPIServer(t *testing.T) {
 	if apiServer == nil {
 		t.Error()
 	}
-}
-
-func TestGetAvailableAPIs(t *testing.T) {
-	expectedList := []api{
-		{OrganizationName: "Test"},
-	}
-
-	actualList := getAvailableAPIs()
-
-	assert.Equal(t, expectedList, actualList)
-}
-
-func mockGetAvailableAPIs() []api {
-	return []api{}
-}
-
-func TestListAPIsHandler(t *testing.T) {
-	logger := zap.NewNop()
-
-	req := httptest.NewRequest("GET", "/list", nil)
-	w := httptest.NewRecorder()
-
-	listAPIsHandler(logger, mockGetAvailableAPIs)(w, req)
-
-	resp := w.Result()
-
-	body, _ := ioutil.ReadAll(resp.Body)
-
-	assert.Equal(t, 200, resp.StatusCode)
-	assert.Equal(t, "application/json", resp.Header.Get("Content-Type"))
-	assert.Equal(t, "[]\n", string(body))
 }
