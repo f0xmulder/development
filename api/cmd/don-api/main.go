2d913b7e5692f3ad0b5278a2b5714bd19cf30bd7
Niels Dequeker <niels@newfront.eu>
2019-02-20 11:16:37 +0100

    #2 Log the address the API is listening to

--- api/cmd/don-api/main.go
+++ api/cmd/don-api/main.go
@@ -46,6 +46,7 @@ func main() {
 	}()
 
 	apiServer := api.NewApiServer(logger)
+	logger.Info("API running and listening on", zap.String("address", options.ListenAddressPlain))
 
 	// Listen on the address provided in the options
 	err = apiServer.ListenAndServe(options.ListenAddressPlain)
