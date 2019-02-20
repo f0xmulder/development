62b27ff10f0ef5237bc0f2ff4b794609d95c67a7
Niels Dequeker <niels@newfront.eu>
2019-02-20 15:15:01 +0100

    #2 Cover scenario where the API returns an error

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,35 +6,50 @@ class App extends Component {
     super(props)
 
     this.state = {
-      apis: []
+      apis: [],
+      error: false,
+      loaded: false
     }
   }
 
   fetchApiList() {
     return fetch('/api/list')
-      .then(response => response.json())
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error('Failed to fetch API list')
+        }
+      })
   }
 
-  componentWillMount() {
+  componentDidMount() {
     this
       .fetchApiList()
       .then(apis => {
-        this.setState({ apis })
+        this.setState({ apis, loaded: true })
+      }, error => {
+        this.setState({ error: true, loaded: true })
+        console.error(error)
       })
   }
 
   render() {
-    const { apis } = this.state
+    const { apis, error, loaded } = this.state
 
     return (
       <div className="App">
         <h1>API overview</h1>
         {
-          apis && apis.length > 0 ?
-            <ul>
-              { apis.map((api, i) => <li key={i}>{ api['organization_name'] }</li>) }
-            </ul> :
-            <p data-test="no-apis-available-message">No APIs available (yet)</p>
+          !loaded ?
+              null :
+              error ?
+                  <p data-test="error-message">Failed loading the available APIs</p> :
+                  apis && apis.length > 0 ?
+                      <ul>
+                        { apis.map((api, i) => <li key={i}>{ api['organization_name'] }</li>) }
+                      </ul> :
+                      <p data-test="no-apis-available-message">No APIs available (yet)</p>
         }
       </div>
     );
--- ui/src/App.test.js
+++ ui/src/App.test.js
@@ -14,7 +14,9 @@ describe('on initialization', () => {
     const wrapper = shallow(<App/>)
     expect(wrapper.instance().fetchApiList).toHaveBeenCalled()
   })
+})
 
+describe('loading the APIs', () => {
   it('should store the available apis as state', () => {
     const apiPromise = Promise.resolve([{test: 'test'}])
     App.prototype.fetchApiList = jest.fn(() => apiPromise)
@@ -34,7 +36,7 @@ describe('listing the available apis', () => {
     const wrapper = shallow(<App/>)
 
     const apis = [{ organization_name: 'Test' }]
-    wrapper.setState({ apis })
+    wrapper.setState({ apis, loaded: true })
 
     listItems = wrapper.find('ul li')
   })
@@ -51,9 +53,34 @@ describe('listing the available apis', () => {
 describe('when no apis are available', () => {
   it('should show a message saying no APIs are available yet', () => {
     const wrapper = shallow(<App/>)
-    wrapper.setState({ apis: [] })
+    wrapper.setState({ apis: [], loaded: true })
     const noApisMessageElement = wrapper.find('[data-test="no-apis-available-message"]')
     expect(noApisMessageElement.exists()).toBe(true)
     expect(noApisMessageElement.text()).toBe('No APIs available (yet)')
+  })
+})
+
+describe('when an error occurred while fetching the apis', () => {
+  it('should set the error state', done => {
+    const thePromise = Promise.reject('reject reason')
+    App.prototype.fetchApiList = jest.fn(() => thePromise)
+
+    const wrapper = shallow(<App />)
+
+    return thePromise
+        .catch(() => {
+          expect(wrapper.state().error).toBe(true)
+          done()
+        })
+  })
+})
+
+describe('when the component is in the error state', () => {
+  it('an error message should be visible', () => {
+    const wrapper = shallow(<App/>)
+    wrapper.setState({ error: true, loaded: true })
+    const noApisMessageElement = wrapper.find('[data-test="error-message"]')
+    expect(noApisMessageElement.exists()).toBe(true)
+    expect(noApisMessageElement.text()).toBe('Failed loading the available APIs')
   })
 })
