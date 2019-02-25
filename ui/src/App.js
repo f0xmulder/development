8f7bf4d9b1bebf236472aa05414a35ed1f236d82
Niels Dequeker <niels@newfront.eu>
2019-02-25 18:49:27 +0100

    #18 Add Navigation + Home as route

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
-import Home from "./pages/Home";
+import Home from './pages/Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 const App = () =>
     <div className="App">
-      <Home/>
+        <Router>
+            <div>
+                <Route exact path="/" component={Home} />
+
+                <hr />
+
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
+            </div>
+        </Router>
     </div>
 
 export default App
