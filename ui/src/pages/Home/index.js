8d7de6ef7f2d63ae485ba569b18546a4ca709c9f
Niels Dequeker <niels@newfront.eu>
2019-02-25 18:45:48 +0100

    #18 Separate API overview into a Homepage

---
+++ ui/src/pages/Home/index.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            apis: [],
+            error: false,
+            loaded: false
+        }
+    }
+
+    fetchApiList() {
+        return fetch('/api/list')
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error('Failed to fetch API list')
+                }
+            })
+    }
+
+    componentDidMount() {
+        this
+            .fetchApiList()
+            .then(apis => {
+                this.setState({ apis, loaded: true })
+            }, error => {
+                this.setState({ error: true, loaded: true })
+                console.error(error)
+            })
+    }
+
+    render() {
+        const { apis, error, loaded } = this.state
+
+        return (
+            <div className="Home">
+                <h1>API overview</h1>
+                {
+                    !loaded ?
+                        null :
+                        error ?
+                            <p data-test="error-message">Failed loading the available APIs</p> :
+                            apis && apis.length > 0 ?
+                                <ul>
+                                    { apis.map((api, i) => <li key={i}>{ api['organization_name'] }</li>) }
+                                </ul> :
+                                <p data-test="no-apis-available-message">No APIs available (yet)</p>
+                }
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ ui/src/pages/Home/index.test.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Home from './index'
+
+it('contains the page title', () => {
+  const wrapper = shallow(<Home/>)
+  expect(wrapper.find('h1').text()).toBe('API overview')
+})
+
+describe('on initialization', () => {
+  it('should fetch the available apis', () => {
+    jest.spyOn(Home.prototype, 'fetchApiList')
+
+    const wrapper = shallow(<Home/>)
+    expect(wrapper.instance().fetchApiList).toHaveBeenCalled()
+  })
+})
+
+describe('loading the APIs', () => {
+  it('should store the available apis as state', () => {
+    const apiPromise = Promise.resolve([{test: 'test'}])
+    Home.prototype.fetchApiList = jest.fn(() => apiPromise)
+
+    const wrapper = shallow(<Home/>)
+    return apiPromise
+        .then(() => {
+          expect(wrapper.state('apis')).toEqual([{ test: 'test'}])
+        })
+  })
+})
+
+describe('listing the available apis', () => {
+  let listItems
+
+  beforeEach(() => {
+    const wrapper = shallow(<Home/>)
+
+    const apis = [{ organization_name: 'Test' }]
+    wrapper.setState({ apis, loaded: true })
+
+    listItems = wrapper.find('ul li')
+  })
+
+  it('should show all available APIs', () => {
+    expect(listItems.length).toBe(1)
+  })
+
+  it('should show the organization name as label', () => {
+    expect(listItems.first().text()).toBe('Test')
+  })
+})
+
+describe('when no apis are available', () => {
+  it('should show a message saying no APIs are available yet', () => {
+    const wrapper = shallow(<Home/>)
+    wrapper.setState({ apis: [], loaded: true })
+    const noApisMessageElement = wrapper.find('[data-test="no-apis-available-message"]')
+    expect(noApisMessageElement.exists()).toBe(true)
+    expect(noApisMessageElement.text()).toBe('No APIs available (yet)')
+  })
+})
+
+describe('when an error occurred while fetching the apis', () => {
+  it('should set the error state', done => {
+    const thePromise = Promise.reject('arbitrary reject reason coming from tests')
+    Home.prototype.fetchApiList = jest.fn(() => thePromise)
+
+    const wrapper = shallow(<Home />)
+
+    return thePromise
+        .catch(() => {
+          expect(wrapper.state().error).toBe(true)
+          done()
+        })
+  })
+})
+
+describe('when the component is in the error state', () => {
+  it('an error message should be visible', () => {
+    const wrapper = shallow(<Home/>)
+    wrapper.setState({ error: true, loaded: true })
+    const noApisMessageElement = wrapper.find('[data-test="error-message"]')
+    expect(noApisMessageElement.exists()).toBe(true)
+    expect(noApisMessageElement.text()).toBe('Failed loading the available APIs')
+  })
+})
