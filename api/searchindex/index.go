494ea409f2e7e83c0854a920dfed19a8317244ff
Niels Dequeker <niels@newfront.eu>
2019-03-11 11:05:05 +0100

    #1 Remove API index before setting up a new index

--- api/searchindex/index.go
+++ api/searchindex/index.go
@@ -4,11 +4,19 @@ import (
 	"github.com/blevesearch/bleve"
 	"gitlab.com/commonground/developer.overheid.nl/api/models"
 	"log"
+	"os"
 )
 
 // Setup search index for Bleve with provided data
 func Setup(indexDirectoryPath string, data []models.API) bleve.Index {
 	apiIndex, errIndex := bleve.Open(indexDirectoryPath)
+
+	// remove existing index, so we can recreate it and add newly added APIs
+	errorRemoveIndex := os.RemoveAll(indexDirectoryPath)
+
+	if errorRemoveIndex != nil {
+		log.Fatal(errIndex)
+	}
 
 	if errIndex == bleve.ErrorIndexPathDoesNotExist {
 		mapping := bleve.NewIndexMapping()
