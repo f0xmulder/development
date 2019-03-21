26f4bd0ae0904ac3254ada0c6149b3e64189cee5
Niels Dequeker <niels@newfront.eu>
2019-03-21 13:42:54 +0100

    #45 About page EN -> NL

--- ui/src/pages/About/index.js
+++ ui/src/pages/About/index.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom'
 
 const About = () =>
     <div className="About container">
-        <h1>About</h1>
+        <h1>Over</h1>
         <p>
-          Developer.overheid.nl provides an overview of all API's that are available within the Dutch government.
-          API's can be added easily by <Link to="/submit-api">submitting a pull request</Link>.
+          Developer.overheid.nl geeft een overzicht van alle API's gerelateerd aan de Nederlandse overheid.
+          API's kunnen makkelijk worden toegevoegd door <Link to="/api-toevoegen">een Merge Request</Link> aan te maken.
         </p>
 
         <p>
-          If you have any questions please contact us at <a href="mailto:developer@overheid.nl">developer@overheid.nl</a>.
-          If you would like to contribute to this portal check out the repository on <a href="https://gitlab.com/commonground/developer.overheid.nl">GitLab</a>.
+          Voor vragen, neem contact op via <a href="mailto:developer@overheid.nl">developer@overheid.nl</a>.
+          De broncode van deze website is te vinden op <a href="https://gitlab.com/commonground/developer.overheid.nl">GitLab</a>.
         </p>
     </div>
 
