01c3619db0097a7ecee9c2d0c247520e3be16839
Niels Dequeker <niels@newfront.eu>
2019-03-26 11:47:36 +0100

    #41 Use nav element for the primary navigation

--- ui/src/components/Navigation/index.js
+++ ui/src/components/Navigation/index.js
@@ -4,11 +4,13 @@ import {Link} from 'react-router-dom'
 import './index.css'
 
 const Navigation = () =>
-    <ul className="Navigation">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/overzicht">Overzicht</Link></li>
-        <li><Link to="/api-toevoegen">API toevoegen</Link></li>
-        <li><Link to="/over">Over</Link></li>
-    </ul>
+    <nav className="Navigation">
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/overzicht">Overzicht</Link></li>
+            <li><Link to="/api-toevoegen">API toevoegen</Link></li>
+            <li><Link to="/over">Over</Link></li>
+        </ul>
+    </nav>
 
 export default Navigation
