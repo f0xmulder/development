803d03fc48884587f17b47c67e2661c6ac95dc93
Niels Dequeker <niels@newfront.eu>
2019-04-02 09:33:30 +0200

    #29 Add tests for the API endpoint /api/apis/:id/implemented-by

--- api/resources/api_test.go
+++ api/resources/api_test.go
@@ -32,6 +32,25 @@ var dummyAPI = models.API{
 	models.APITermsOfUse{},
 }
 
+var dummyImplementationOfReferenceAPI = models.API{
+	"test-implementation-of-reference-api",
+	"Test Description",
+	"Test Organization Name",
+	"Test Service Name",
+	"Test API URL",
+	"Test Specification Type",
+	"Test Specification URL",
+	"Test Documentation URL",
+	[]models.Tag{},
+	[]string{},
+	models.APIContactDetails{},
+	false,
+	map[string][]string{
+		"test-reference-api": {RELATION_TYPE_REFERENCE_IMPLEMENTATION},
+	},
+	models.APITermsOfUse{},
+}
+
 func mockAPIDirectoryReaderNoResults(directory string) ([]models.API, error) {
 	return []models.API{}, nil
 }
@@ -175,6 +194,78 @@ func TestAPIGet(t *testing.T) {
 			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
 
 			apiResource.Get(w, req)
+			resp := w.Result()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			if string(body) != tc.wantResponseBody {
+				t.Errorf("got body %s, want %s", string(body), tc.wantResponseBody)
+			}
+
+			if resp.StatusCode != tc.wantStatusCode {
+				t.Errorf("got status code %d, want %d", resp.StatusCode, tc.wantStatusCode)
+			}
+
+			if resp.Header.Get("Content-Type") != tc.wantContentType {
+				t.Errorf("got content-type %s, want %s", resp.Header.Get("Content-Type"), tc.wantContentType)
+			}
+		})
+	}
+}
+
+func TestAPIImplementedBy(t *testing.T) {
+	testCases := []struct {
+		apiID                       string
+		rootDirectoryAPIDefinitions string
+		wantStatusCode              int
+		wantContentType             string
+		wantResponseBody            string
+		mockAPIDirectoryReader      func(path string) ([]models.API, error)
+	}{
+		{
+			"test-api-name",
+			"./test-data/valid",
+			200,
+			"application/json",
+			"[]\n",
+			func(directory string) ([]models.API, error) {
+				return []models.API{
+					dummyAPI,
+				}, nil
+			},
+		},
+		{
+			"test-reference-api",
+			"./test-data/valid",
+			200,
+			"application/json",
+			"[{\"id\":\"test-implementation-of-reference-api\",\"description\":\"Test Description\",\"organization_name\":\"Test Organization Name\",\"service_name\":\"Test Service Name\",\"api_url\":\"Test API URL\",\"api_specification_type\":\"Test Specification Type\",\"specification_url\":\"Test Specification URL\",\"documentation_url\":\"Test Documentation URL\",\"tags\":[],\"badges\":[],\"contact\":{\"email\":\"\",\"phone\":\"\",\"fax\":\"\",\"chat\":\"\",\"url\":\"\"},\"is_reference_implementation\":false,\"relations\":{\"test-reference-api\":[\"reference-implementation\"]},\"terms_of_use\":{\"government_only\":false,\"pay_per_use\":false,\"uptime_guarantee\":0,\"support_response_time\":\"\"}}]\n",
+			func(directory string) ([]models.API, error) {
+				return []models.API{
+					dummyImplementationOfReferenceAPI,
+				}, nil
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		url := strings.Join([]string{"/api/apis", tc.apiID, "implemented-by"}, "/")
+		apiResource := APIResource{
+			zap.NewNop(),
+			tc.rootDirectoryAPIDefinitions,
+			nil,
+			tc.mockAPIDirectoryReader,
+		}
+
+		t.Run(fmt.Sprintf("%s", url), func(t *testing.T) {
+			req := httptest.NewRequest("GET", url, nil)
+			w := httptest.NewRecorder()
+
+			ctx := chi.NewRouteContext()
+			ctx.URLParams.Add("id", tc.apiID)
+
+			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+			apiResource.ListImplementedBy(w, req)
 			resp := w.Result()
 			body, _ := ioutil.ReadAll(resp.Body)
 
