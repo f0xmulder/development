4dfbcc5abed7f75b045a1810e4f3a50164a3feac
Niels Dequeker <niels@newfront.eu>
2019-04-02 14:31:16 +0200

    #52 Remove console.log

--- e2e-tests/jest-puppeteer.config.js
+++ e2e-tests/jest-puppeteer.config.js
@@ -5,8 +5,6 @@ const getExecutablePath = () =>
         'google-chrome-unstable' :
         puppeteer.executablePath()
 
-console.log(!!process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD, process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD, getExecutablePath())
-
 // Prevent downloading Chrome for every E2E CI job.
 // We can use the pre-installed 'google-chrome-unstable' executable instead.
 // Via https://github.com/GoogleChrome/puppeteer/blob/de82b87cfa1c637787a09b90266318905ae16f42/docs/troubleshooting.md#running-puppeteer-in-docker
