978315e3a57932390c198e519d0a14f137f4064c
Bart Jeukendrup <bart@jeukendrup.nl>
2019-04-08 16:41:21 +0200

    Refactored search form

--- ui/src/pages/Home/index.js
+++ ui/src/pages/Home/index.js
@@ -1,131 +1,46 @@
 import React, {Component} from 'react'
-import { object } from 'prop-types'
-import APIList from '../../components/APIList';
 import TagListContainer from "../../components/TagListContainer";
 
 import './index.css'
-import Search from "../../components/Icons/Search";
+import SearchIcon from "../../components/Icons/Search";
 
-export const mapApisForQueryResponseToApis = response => {
-    const hits = response.hits
-    return hits.map(hit => hit.fields)
-}
-
-class Home extends Component {
+export default class Home extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            apis: [],
-            error: false,
-            loaded: false
+            searchInput: ''
         }
 
-        this.onInputChangedHandler = this.onInputChangedHandler.bind(this)
+        this.onChangeSearchInput = this.onChangeSearchInput.bind(this)
+        this.onSubmit = this.onSubmit.bind(this)
     }
 
-    fetchApisForQuery(query) {
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                query: {match: query},
-                fields: ['id', 'service_name', 'organization_name']
-            })
-        }
-
-        return fetch('/api/apis/search', requestOptions)
-            .then(response => {
-                if (response.ok) {
-                    return response.json()
-                } else {
-                    throw new Error(`Er ging iets fout tijdens het ophalen van API's voor de zoekterm '${query}'`)
-                }
-            })
-            .then(response => mapApisForQueryResponseToApis(response))
+    onChangeSearchInput(e) {
+        this.setState({ searchInput: e.target.value })
     }
 
-    loadApisForQuery(query) {
-        return this
-            .fetchApisForQuery(query)
-            .then(apis => {
-                this.setState({ apis, loaded: true })
-            }, error => {
-                this.setState({ error: true, loaded: true })
-                console.error(error)
-            })
-    }
-
-    componentWillUpdate(nextProps) {
-        const { location: { search } } = nextProps
-        const { location: { search: prevHome } } = this.props
-
-        const urlParams = new URLSearchParams(search)
-        const prevUrlParams = new URLSearchParams(prevHome)
-
-        if (prevUrlParams.get('query') === urlParams.get('query')) {
-            return
-        }
-
-        this.loadApisForQuery(urlParams.get('query'))
-    }
-
-    onInputChangedHandler(event) {
-        const query = event.currentTarget.value
-        this.updateQueryString('query', query)
-    }
-
-    updateQueryString(key, value) {
-        const currentQueryString = this.props.location.search
-        const urlParams = new URLSearchParams(currentQueryString)
-        urlParams.set(key, value)
-        this.props.history.push(`?${urlParams.toString()}`);
-    }
-
-    componentDidMount() {
-        const { location: { search } } = this.props
-        const urlParams = new URLSearchParams(search)
-        const query = urlParams.get('query')
-
-        if (!query) {
-            return
-        }
-
-        this.loadApisForQuery(query)
+    onSubmit(e) {
+        e.preventDefault()
+        const { history } = this.props
+        const urlParams = new URLSearchParams({ q: this.state.searchInput })
+        history.push(`/overzicht?${urlParams.toString()}`)
     }
 
     render() {
-        const { apis, error, loaded } = this.state
-        const { search } = this.props.location
-        const urlParams = new URLSearchParams(search)
-        const query = urlParams.get('query')
-
         return (
             <div className="Home container">
                 <div className="Home__Search">
                     <h1>Een incompleet overzicht van alle API’s binnen de Nederlandse overheid</h1>
 
                     <div className="search-box">
-                        <label htmlFor="search-query" aria-label="Search query">
-                            <input type="text" id="search-query" placeholder="Zoeken naar een API" defaultValue={query} onChange={event => this.onInputChangedHandler(event)}/>
+                        <label htmlFor="searchInput" aria-label="Search query">
+                            <form method="POST" onSubmit={this.onSubmit} data-test="search-form">
+                                <input type="text" name="q" id="searchInput" placeholder="Zoeken naar een API" data-test="search-input" onChange={this.onChangeSearchInput} value={this.state.searchInput} />
+                            </form>
                         </label>
-                        <Search/>
+                        <SearchIcon/>
                     </div>
-
-                    {
-                        !loaded ?
-                            null :
-                            error ?
-                                <p data-test="error-message">Er ging iets fout tijdens het laden van de API's</p> :
-                                apis && apis.length > 0 ?
-                                    <div>
-                                        <APIList apis={apis}/>
-                                    </div>
-                                    :
-                                    <p data-test="no-apis-found-message">Geen API's gevonden.</p>
-                    }
                 </div>
 
                 <div className="Home__Tags">
@@ -136,14 +51,3 @@ class Home extends Component {
         );
     }
 }
-
-Home.propTypes = {
-    location: object
-}
-
-Home.defaultProps = {
-    location: { search: {} }
-}
-
-
-export default Home
--- ui/src/pages/Home/index.test.js
+++ ui/src/pages/Home/index.test.js
@@ -1,144 +1,33 @@
 import React from 'react'
-import {shallow} from 'enzyme'
-import Search, {mapApisForQueryResponseToApis} from './index'
+import {shallow, mount} from 'enzyme'
+import Search from './index'
 
-const dummyAPI = {
-  id: 'test-api.json',
-  organization_name: 'Organization Name',
-  service_name: 'Service Name'
-}
+const historyPush = jest.fn()
 
 describe('Search', () => {
-  afterEach(() => jest.clearAllMocks())
-
   it('contains the page title', () => {
     const wrapper = shallow(<Search/>)
     expect(wrapper.find('h1').text()).toBe('Een incompleet overzicht van alle API’s binnen de Nederlandse overheid')
   })
 
-  describe('mapping api response to api list', () => {
-    it('should return a list of apis', () => {
-      const input = {
-        hits: [{
-          fields: {
-            id: 'foo'
-          }
-        }]
-      }
-      const response = mapApisForQueryResponseToApis(input)
-      expect(response).toEqual([{ id: 'foo' }])
-    })
-  })
+  describe('when submitting the form', () => {
+    it('should call the onSubmit handler', () => {
+      const onSubmitSpy = jest.spyOn(Search.prototype, 'onSubmit')
+      const wrapper = mount(<Search history={{ push: historyPush }} />)
+      const searchForm = wrapper.find('[data-test="search-form"]')
+      searchForm.simulate('submit', { preventDefault: () => {} })
 
-  describe('on initialization', () => {
-    describe('when a query is set', () => {
-      it('should fetch the available apis', () => {
-        jest.spyOn(Search.prototype, 'fetchApisForQuery')
-
-        const wrapper = shallow(<Search location={({search: { query: 'foo' }})}/>)
-        expect(wrapper.instance().fetchApisForQuery).toHaveBeenNthCalledWith(1, 'foo')
-      })
+      expect(onSubmitSpy).toHaveBeenCalled()
     })
 
-    describe('when the query is not set', () => {
-      it('should not fetch the available apis', () => {
-        jest.spyOn(Search.prototype, 'fetchApisForQuery')
+    it('should push to history with the query parameter', () => {
+      const wrapper = mount(<Search history={{ push: historyPush }} />)
+      wrapper.setState({ searchInput: "42" })
 
-        const wrapper = shallow(<Search/>)
-        expect(wrapper.instance().fetchApisForQuery).toHaveBeenCalledTimes(0)
-      })
-    })
-  })
+      const searchForm = wrapper.find('[data-test="search-form"]')
+      searchForm.simulate('submit', { preventDefault: () => {} })
 
-  describe('loading the APIs', () => {
-    it('should store the returned apis as state', () => {
-      const apiPromise = Promise.resolve([dummyAPI])
-      Search.prototype.fetchApisForQuery = jest.fn(() => apiPromise)
-
-      const wrapper = shallow(<Search/>)
-      return wrapper.instance().loadApisForQuery('foo')
-          .then(() => {
-            expect(wrapper.state('apis')).toEqual([dummyAPI])
-          })
-    })
-  })
-
-  describe('listing the found apis', () => {
-    let apiList
-
-    beforeEach(() => {
-      const wrapper = shallow(<Search/>)
-      wrapper.setState({ apis: [dummyAPI], loaded: true })
-      apiList = wrapper.find('APIList')
-    })
-
-    it('should show the list of APIs', () => {
-      expect(apiList.exists()).toBe(true)
-    })
-  })
-
-  describe('when no apis are available', () => {
-    it('should show a message saying no APIs have been found', () => {
-      const wrapper = shallow(<Search/>)
-      wrapper.setState({ apis: [], loaded: true })
-      const noApisMessageElement = wrapper.find('[data-test="no-apis-found-message"]')
-      expect(noApisMessageElement.exists()).toBe(true)
-    })
-  })
-
-  describe('when an error occurred while fetching the apis', () => {
-    it('should set the error state', done => {
-      const thePromise = Promise.reject('arbitrary reject reason coming from tests')
-      Search.prototype.fetchApisForQuery = jest.fn(() => thePromise)
-
-      const wrapper = shallow(<Search location={({search: { query: 'foo' }})} />)
-
-      return thePromise
-          .catch(() => {
-            expect(wrapper.state().error).toBe(true)
-            done()
-          })
-    })
-  })
-
-  describe('when the component is in the error state', () => {
-    it('an error message should be visible', () => {
-      const wrapper = shallow(<Search/>)
-      wrapper.setState({ error: true, loaded: true })
-      const noApisMessageElement = wrapper.find('[data-test="error-message"]')
-      expect(noApisMessageElement.exists()).toBe(true)
-    })
-  })
-
-  describe('the input change handler', () => {
-    it('should update the query string with the input value', () => {
-      jest.spyOn(Search.prototype, 'updateQueryString')
-      const wrapper = shallow(<Search location={({})} history={({push: () => {}})}/>)
-
-      const event = {currentTarget: {value: 'my-query'}}
-      wrapper.instance().onInputChangedHandler(event)
-
-      expect(wrapper.instance().updateQueryString).toHaveBeenCalledWith('query', 'my-query')
-    })
-  })
-
-  describe('updating the query string', () => {
-    it('should update the query string for the provided key and value', () => {
-      const pushSpy = jest.fn()
-      const wrapper = shallow(<Search history={({ push: pushSpy })}/>)
-      wrapper.instance().updateQueryString('query', 'my-updated-query')
-      expect(pushSpy).toHaveBeenCalledWith(`?query=my-updated-query`)
-    })
-
-    it('should preserve existing key-value pairs', () => {
-      const pushSpy = jest.fn()
-        const wrapper = shallow(<Search history={({push: pushSpy})}
-                                        location={({search: new URLSearchParams('?foo=bar')})}/>)
-
-      const event = {currentTarget: {value: 'my-query'}}
-      wrapper.instance().onInputChangedHandler(event)
-
-      expect(pushSpy).toHaveBeenCalledWith(`?foo=bar&query=my-query`)
+      expect(historyPush).toHaveBeenCalledWith(`/overzicht?q=42`)
     })
   })
 })
