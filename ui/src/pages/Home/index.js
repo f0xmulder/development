25b28313e4dc606b3282559ac914cc325b4831a9
Niels Dequeker <niels@newfront.eu>
2019-04-24 16:14:55 +0200

    refactor(ui): convert Homepage to pure function

--- ui/src/pages/Home/index.js
+++ ui/src/pages/Home/index.js
@@ -4,49 +4,23 @@ import {Search} from '@commonground/design-system'
 
 import './index.css'
 
-export default class Home extends Component {
-    constructor(props) {
-        super(props)
+export default () =>
+  <div className="Home container">
+      <div className="Home__Search">
+          <h1>Een incompleet overzicht van alle API’s binnen de Nederlandse overheid</h1>
 
-        this.state = {
-            searchInputValue: ''
-        }
+          <form method="GET" action="/overzicht">
+              <div className="search-box">
+                  <label htmlFor="searchInput" aria-label="Zoekterm">
+                    <Search placeholder="Zoeken naar een API" inputName="q" inputId="searchInput" />
+                  </label>
+              </div>
+          </form>
+      </div>
 
-        this.onSearchInputValueChanged = this.onSearchInputValueChanged.bind(this)
-        this.onSubmit = this.onSubmit.bind(this)
-    }
+      <div className="Home__Tags">
+          <p>Categorieën</p>
+          <TagListContainer />
+      </div>
+  </div>
 
-    onSearchInputValueChanged(e) {
-        this.setState({ searchInputValue: e.target.value })
-    }
-
-    onSubmit(e) {
-        e.preventDefault()
-        const { history } = this.props
-        const urlParams = new URLSearchParams({ q: this.state.searchInputValue })
-        history.push(`/overzicht?${urlParams.toString()}`)
-    }
-
-    render() {
-        return (
-            <div className="Home container">
-                <div className="Home__Search">
-                    <h1>Een incompleet overzicht van alle API’s binnen de Nederlandse overheid</h1>
-
-                    <form method="GET" action="/overzicht">
-                        <div className="search-box">
-                            <label htmlFor="searchInput" aria-label="Zoekterm">
-                              <Search placeholder="Zoeken naar een API" inputName="q" inputId="searchInput" />
-                            </label>
-                        </div>
-                    </form>
-                </div>
-
-                <div className="Home__Tags">
-                    <p>Categorieën</p>
-                    <TagListContainer />
-                </div>
-            </div>
-        );
-    }
-}
