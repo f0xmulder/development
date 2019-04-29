9ecb034d98379c469ac9f1a00bfc1982a028715f
Niels Dequeker <niels@newfront.eu>
2019-04-29 16:47:51 +0200

    refactor(ui): extract calculateGrade from the Grade component

--- ui/src/components/Grade/index.js
+++ ui/src/components/Grade/index.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import {shape,bool} from 'prop-types'
 
-class Grade extends Component {
-    calculateGrade() {
-        const values = Object.values(this.props.scores)
-        const percentage = values.reduce((total, value) => value ? (total+1) : total, 0) / values.length
-        return Math.round(percentage * 10 * 10) / 10
-    }
+export const calculateGrade = (scores) => {
+    const values = Object.values(scores)
+    const percentage = values.reduce((total, value) => value ? (total+1) : total, 0) / values.length
+    return Math.round(percentage * 10 * 10) / 10
+}
 
+class Grade extends Component {
     render() {
-        return (
-            <React.Fragment>
-                {this.calculateGrade()}
-            </React.Fragment>
-        )
+      const { scores } = this.props
+
+      return (
+          <React.Fragment>
+              {calculateGrade(scores)}
+          </React.Fragment>
+      )
     }
 }
 
--- ui/src/components/Grade/index.test.js
+++ ui/src/components/Grade/index.test.js
@@ -1,20 +1,32 @@
-import {shallow} from "enzyme/build";
-import Grade from "./index"
 import React from "react";
-
-const testCases = [
-    { scores: { "has_documentation": true, "has_specification": false, "has_contact_details": false, "provides_sla": false }, grade: '2.5' },
-    { scores: { "has_documentation": true, "has_specification": true, "has_contact_details": false, "provides_sla": true }, grade: '7.5' },
-    { scores: { "has_documentation": true, "has_specification": false, "has_contact_details": true, "provides_sla": false }, grade: '5' }
-]
+import {shallow} from "enzyme/build";
+import Grade, {calculateGrade} from "./index"
 
 describe('Grade', () => {
-    let wrapper
-
-    it('should display the correct score', () => {
-        testCases.forEach((testCase) => {
-            wrapper = shallow(<Grade scores={testCase.scores} />)
-            expect(wrapper.text()).toBe(testCase.grade)
-        })
+    it('should display the calculated score', () => {
+      const scores = {
+        "has_documentation": true,
+        "has_specification": true,
+        "has_contact_details": true,
+        "provides_sla": true
+      }
+      const wrapper = shallow(<Grade scores={scores} />)
+      expect(wrapper.text()).toBe('10')
     })
 })
+
+describe('calculating the grade from scores', () => {
+  it('should equal the amount of scores with value true on the total amount of scores', () => {
+    const testCases = [
+      {scores: {"foo": true }, grade: 10},
+      {scores: {"foo": true, "bar": false }, grade: 5},
+      {scores: {"foo": true, "bar": false, "baz": false, "foobar": false }, grade: 2.5}
+    ]
+
+    
+    testCases.forEach(({scores, grade}) => {
+      expect(calculateGrade(scores)).toBe(grade)
+    })
+  })
+})
+
