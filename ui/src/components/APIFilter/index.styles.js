f7e81650a085b4f67b9d9308a2a61618dfc602cb
Niels Dequeker <niels@newfront.eu>
2019-05-02 14:35:17 +0200

    feat(ui): style title of the APIFilter

--- ui/src/components/APIFilter/index.styles.js
+++ ui/src/components/APIFilter/index.styles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 export const StyledAPIFilter = styled.div`
   h2 {
-   font-size: 1.3em; 
+   font-size: 12px;
+   color: #A3AABF;
+   font-weight: 600;
   }
 `
