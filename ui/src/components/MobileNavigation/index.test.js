30e46e849d21461bcb75773f8887869ec8919610
Niels Dequeker <niels@newfront.eu>
2019-05-03 10:41:40 +0200

    fix(ui): make test for MobileNavigation pass #60

--- ui/src/components/MobileNavigation/index.test.js
+++ ui/src/components/MobileNavigation/index.test.js
@@ -1,30 +1,31 @@
 import React from 'react'
-import {shallow} from 'enzyme/build'
+import {shallow} from 'enzyme'
 import MobileNavigation from './index'
+import {Menu, Link} from './index.styles'
 
 describe('the MobileNavigation', () => {
-    let menu
+  let menu
 
-    beforeAll(() => {
-        const wrapper = shallow(<MobileNavigation/>)
-        menu = wrapper.find('Menu')
-    })
+  beforeAll(() => {
+    const wrapper = shallow(<MobileNavigation/>)
+    menu = wrapper.find(Menu)
+  })
 
-    it('should contain a link to the Home page', () => {
-        const homeLink = menu.childAt(0).find('Link')
-        expect(homeLink.props().children).toBe('Home')
-        expect(homeLink.props().to).toBe('/')
-    })
+  it('should contain a link to the Home page', () => {
+    const homeLink = menu.childAt(0).find(Link)
+    expect(homeLink.props().children).toBe('Home')
+    expect(homeLink.props().to).toBe('/')
+  })
 
-    it('should contain a link to the Overview page', () => {
-        const homeLink = menu.childAt(1).find('Link')
-        expect(homeLink.props().children).toBe('Overzicht')
-        expect(homeLink.props().to).toBe('/overzicht')
-    })
+  it('should contain a link to the Overview page', () => {
+    const homeLink = menu.childAt(1).find(Link)
+    expect(homeLink.props().children).toBe('Overzicht')
+    expect(homeLink.props().to).toBe('/overzicht')
+  })
 
-    it('should contain a link to the About page', () => {
-        const homeLink = menu.childAt(2).find('Link')
-        expect(homeLink.props().children).toBe('Over')
-        expect(homeLink.props().to).toBe('/over')
-    })
+  it('should contain a link to the About page', () => {
+    const homeLink = menu.childAt(2).find(Link)
+    expect(homeLink.props().children).toBe('Over')
+    expect(homeLink.props().to).toBe('/over')
+  })
 })
