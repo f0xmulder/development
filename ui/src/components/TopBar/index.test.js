57eed60d06a4345a56e204e6334ed602fc3bd329
Niels Dequeker <niels@newfront.eu>
2019-05-06 16:56:46 +0200

    test(ui): add Homepage link for the TopBar component

--- ui/src/components/TopBar/index.test.js
+++ ui/src/components/TopBar/index.test.js
@@ -3,28 +3,35 @@ import {shallow} from 'enzyme/build'
 import TopBar from './index'
 
 describe('the TopBar', () => {
+    let wrapper
     let navigation
 
     beforeAll(() => {
-        const wrapper = shallow(<TopBar/>)
+        wrapper = shallow(<TopBar/>)
         navigation = wrapper.find('Styled(Navigation)')
     })
 
+    it('should contain a link to the Home page', () => {
+      const homeLink = wrapper.find('Styled(Link)')
+      expect(homeLink.props().children).toBe('developer.overheid.nl')
+      expect(homeLink.props().to).toBe('/')
+    })
+
     it('should contain a link to the Overview page', () => {
-        const homeLink = navigation.childAt(0).find('NavLink')
-        expect(homeLink.props().children).toBe('Overzicht')
-        expect(homeLink.props().to).toBe('/overzicht')
+        const overviewLink = navigation.childAt(0).find('NavLink')
+        expect(overviewLink.props().children).toBe('Overzicht')
+        expect(overviewLink.props().to).toBe('/overzicht')
     })
 
     it('should contain a link to the Submit API page', () => {
-        const homeLink = navigation.childAt(1).find('NavLink')
-        expect(homeLink.props().children).toBe('API toevoegen')
-        expect(homeLink.props().to).toBe('/api-toevoegen')
+      const submitAPILink = navigation.childAt(1).find('NavLink')
+        expect(submitAPILink .props().children).toBe('API toevoegen')
+        expect(submitAPILink .props().to).toBe('/api-toevoegen')
     })
 
     it('should contain a link to the About page', () => {
-        const homeLink = navigation.childAt(2).find('NavLink')
-        expect(homeLink.props().children).toBe('Over')
-        expect(homeLink.props().to).toBe('/over')
+        const aboutLink = navigation.childAt(2).find('NavLink')
+        expect(aboutLink.props().children).toBe('Over')
+        expect(aboutLink.props().to).toBe('/over')
     })
 })
