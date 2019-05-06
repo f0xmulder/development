767397663568f10fe5178af059553449720aeb92
Fabian Buijing <fabianbuijing@gmail.com>
2019-05-06 16:14:12 +0200

    refactor(ui): remove unnecessary svg attributes

--- ui/src/components/MobileNavigation/index.js
+++ ui/src/components/MobileNavigation/index.js
@@ -3,7 +3,7 @@ import { NavButton, Navigation, Menu, Link, Divider } from './index.styles'
 
 const HamburgerIcon = ({ ...props }) =>
   <svg viewBox="0 0 18 12" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M0,12 L18,12 L18,10 L0,10 L0,12 Z M0,7 L18,7 L18,5 L0,5 L0,7 Z M0,0 L0,2 L18,2 L18,0 L0,0 Z" id="Shape" fill="#676D80" fillRule="nonzero"></path>
+    <path d="M0,12 L18,12 L18,10 L0,10 L0,12 Z M0,7 L18,7 L18,5 L0,5 L0,7 Z M0,0 L0,2 L18,2 L18,0 L0,0 Z" fill="#676D80"></path>
   </svg>
 
 HamburgerIcon.defaultProps = {
@@ -13,7 +13,7 @@ HamburgerIcon.defaultProps = {
 
 const CrossIcon = ({ ...props }) =>
   <svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <polygon id="Path" fill="#676D80" points="14 1.41 12.59 0 7 5.59 1.41 0 0 1.41 5.59 7 0 12.59 1.41 14 7 8.41 12.59 14 14 12.59 8.41 7"></polygon>
+    <polygon fill="#676D80" points="14 1.41 12.59 0 7 5.59 1.41 0 0 1.41 5.59 7 0 12.59 1.41 14 7 8.41 12.59 14 14 12.59 8.41 7"></polygon>
   </svg>
 
 CrossIcon.defaultProps = {
