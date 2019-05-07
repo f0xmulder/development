0650176bd4a90f4f121e42b94ed4b8cb2db53e79
Niels Dequeker <niels@newfront.eu>
2019-05-07 12:27:10 +0200

    refactor(ui): removed superfluous PlusIcon component #61

--- ui/src/components/TopBar/index.js
+++ ui/src/components/TopBar/index.js
@@ -3,16 +3,6 @@ import StyledHeader, { StyledTitle, StyledNavigation } from './index.styles'
 import {Navigation} from '@commonground/design-system'
 import {NavLink} from 'react-router-dom'
 
-const PlusIcon = ({ ...props }) =>
-  <svg viewBox="0 0 8 8" xmlns="http://www.w3.org/2000/svg" {...props}>
-    <path d="M8 5H5v3H3V5H0V3h3V0h2v3h3z" fill="#FFF" fillRule="evenodd"/>
-  </svg>
-
-PlusIcon.defaultProps = {
-  width: '8px',
-  height: '8px'
-}
-
 export default () =>
   <StyledHeader>
     <StyledTitle to="/">developer.overheid.nl</StyledTitle>
