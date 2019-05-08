6191589c8c8a4a0b064fb851c937b5452fd41bd4
Niels Dequeker <niels@newfront.eu>
2019-05-08 23:01:57 +0200

    style(ui): indentation

--- ui/src/components/APIFilter/index.styles.js
+++ ui/src/components/APIFilter/index.styles.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 
 export const StyledAPIFilter = styled.div`
-&:not(:last-child) {
-  margin-bottom: 24px;
-}
+  &:not(:last-child) {
+    margin-bottom: 24px;
+  }
+
   h2 {
     margin: 0;
-   font-size: 12px;
-   color: #676D80;
-   font-weight: 600;
+    font-size: 12px;
+    color: #676D80;
+    font-weight: 600;
   }
 `
