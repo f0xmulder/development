82c0b6fcb67d4ad6756d58b34f7c51191a22a1e5
Fabian Buijing <fabianbuijing@gmail.com>
2019-05-09 15:41:00 +0200

    feat(ui): add styling for disabled filters + add pointer cursor for active filters

--- ui/src/components/CheckboxGroupField/index.styles.js
+++ ui/src/components/CheckboxGroupField/index.styles.js
@@ -6,10 +6,28 @@ export const StyledCheckboxGroupField = styled.div`
     position: relative;
     display: flex;
     margin-top: .3rem;
+    user-select: none;
 
     input {
       flex: 0 0 auto;
       margin: 3px 8px 0 0;
+
+      &[disabled] {
+          opacity: 0.5;
+
+        + label {
+          opacity: 0.5;
+          text-decoration: line-through;
+        }
+      }
+
+      &:not([disabled]) {
+        cursor: pointer;
+
+        + label {
+          cursor: pointer;
+        }
+      }
     }
 
     label {
