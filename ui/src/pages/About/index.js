b14145e5a835867a64c46ec5baa5df8d8013f849
Niels <niels@newfront.eu>
2019-05-10 09:38:30 +0000

    chore(ui): removed unused import #82

--- ui/src/pages/About/index.js
+++ ui/src/pages/About/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import {StyledAbout, StyledPageTitle} from './index.styles'
 import PageContentCard from '../../components/PageContentCard'
 
