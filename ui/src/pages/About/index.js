fd50b28e613c7c17ecb29aeb083ae1436838db6c
Niels <niels@newfront.eu>
2019-05-10 09:34:20 +0000

    feat(ui): updated content for the About page
    Closes #82

--- ui/src/pages/About/index.js
+++ ui/src/pages/About/index.js
@@ -9,12 +9,31 @@ const About = () =>
 
       <PageContentCard>
         <p>
-          Developer.overheid.nl geeft een overzicht van alle API's gerelateerd aan de Nederlandse overheid.
-          API's kunnen makkelijk worden toegevoegd door een <Link to="/api-toevoegen/formulier">formuler</Link> in te vullen of een <Link to="/api-toevoegen">Merge Request</Link> aan te maken.
+            De website developer.overheid.nl is een wegwijzer naar de Application Programming Interfaces (API’s) die (semi-)overheidsorganisaties in Nederland aanbieden.
+        </p>
+        <p>
+            Deze website is ‘permanent beta’, en zal worden aangepast naar de behoeften van gebruikers. Wensen en opmerkingen kunnen via het contactadres worden doorgegeven.
+        </p>
+        <p>
+            Deze website is een initiatief van het ministerie van Binnenlandse Zaken en Koninkrijksrelaties in samenwerking met de Vereniging van Nederlandse Gemeenten / VNG Realisatie
+        </p>
+        
+        <h3>Verantwoordelijkheid</h3>
+        <p>
+            De informatie die op deze site wordt aangeboden is afkomstig van andere overheidsorganisaties. Deze organisaties zijn zelf verantwoordelijk voor hun diensten en gegevens. 
         </p>
 
+        <h3>Persoonsgegevens en cookies</h3>
         <p>
-          Voor vragen, neem contact op via <a href="mailto:developer@overheid.nl">developer@overheid.nl</a>.
+            Deze website verzamelt geen persoonsgegevens, zet geen cookies en verzamelt geen persoonsgebonden analytische informatie. 
+        </p>
+
+        <h3>Toegankelijkheid</h3>
+        <p>
+            Deze site is ontworpen om te voldoen aan de vereisten van WCAG 2.0 en de Nederlandse extensies daarop. Omdat aanmelden en omschrijven van de API’s een open systeem is, kunnen we niet garanderen dat de site altijd aan de redactionele vereisten voldoet. Verbeterpunten kunnen worden doorgegeven via het contactadres.
+        </p>
+        
+        <p>
           De broncode van deze website is te vinden op <a href="https://gitlab.com/commonground/developer.overheid.nl">GitLab</a>.
         </p>
       </PageContentCard>
