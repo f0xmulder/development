06a80041f78110a78ef8f86ce3c655ce7937e8ea
Fabian Buijing <fabianbuijing@gmail.com>
2019-05-10 13:17:02 +0200

    feat(ui): add headings + better padding + styling for abbreviations

--- ui/src/pages/About/index.js
+++ ui/src/pages/About/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {StyledAbout, StyledPageTitle} from './index.styles'
-import PageContentCard from '../../components/PageContentCard'
+import {StyledCard, StyledAbout, StyledPageTitle} from './index.styles'
 
 const About = () =>
     <StyledAbout>
       <StyledPageTitle>Over</StyledPageTitle>
 
-      <PageContentCard>
+      <StyledCard>
         <p>
             De website developer.overheid.nl is een wegwijzer naar de <abbr title="Application Programming Interfaces">API’s</abbr> die (semi-)overheidsorganisaties in Nederland aanbieden.
         </p>
@@ -16,26 +15,26 @@ const About = () =>
         <p>
             Deze website is een initiatief van het ministerie van Binnenlandse Zaken en Koninkrijksrelaties in samenwerking met de Vereniging van Nederlandse Gemeenten / VNG Realisatie
         </p>
-        
-        <p>Verantwoordelijkheid</p>
+
+        <strong>Verantwoordelijkheid</strong>
         <p>
-            De informatie die op deze site wordt aangeboden is afkomstig van andere overheidsorganisaties. Deze organisaties zijn zelf verantwoordelijk voor hun diensten en gegevens. 
+            De informatie die op deze site wordt aangeboden is afkomstig van andere overheidsorganisaties. Deze organisaties zijn zelf verantwoordelijk voor hun diensten en gegevens.
         </p>
 
-        <p>Persoonsgegevens en cookies</p>
+        <strong>Persoonsgegevens en cookies</strong>
         <p>
-            Deze website verzamelt geen persoonsgegevens, zet geen cookies en verzamelt geen persoonsgebonden analytische informatie. 
+            Deze website verzamelt geen persoonsgegevens, zet geen cookies en verzamelt geen persoonsgebonden analytische informatie.
         </p>
 
-        <p>Toegankelijkheid</p>
+        <strong>Toegankelijkheid</strong>
         <p>
             Deze site is ontworpen om te voldoen aan de vereisten van WCAG 2.0 en de Nederlandse extensies daarop. Omdat aanmelden en omschrijven van de API’s een open systeem is, kunnen we niet garanderen dat de site altijd aan de redactionele vereisten voldoet. Verbeterpunten kunnen worden doorgegeven via het contactadres.
         </p>
-        
+
         <p>
           De broncode van deze website is te vinden op <a href="https://gitlab.com/commonground/developer.overheid.nl">GitLab</a>.
         </p>
-      </PageContentCard>
+      </StyledCard>
     </StyledAbout>
 
 export default About
--- ui/src/pages/About/index.styles.js
+++ ui/src/pages/About/index.styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Card } from '@commonground/design-system'
 
 export const StyledAbout = styled.div`
   max-width: 600px;
@@ -11,5 +12,25 @@ export const StyledPageTitle = styled.h1`
   font-weight: 800;
   text-align: center;
   margin-bottom: 32px;
+`
+
+export const StyledCard = styled(Card)`
+  padding: 24px 24px;
+  margin-bottom: 40px;
+
+  @media screen and (min-width: 768px) {
+    padding: 40px 56px;
+    margin-bottom: 80px;
+  }
+
+  a {
+    color: #2961FF;
+    text-decoration: none;
+  }
+
+  abbr {
+    text-decoration: none;
+    border-bottom: 1px dotted #A3AABF;
+  }
 `
 
