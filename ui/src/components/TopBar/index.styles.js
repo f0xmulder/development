d860c98697c07eac283b59fc3af1ff3f62441f08
Niels Dequeker <niels@newfront.eu>
2019-05-14 13:11:38 +0200

    fix(ui): linting

--- ui/src/components/TopBar/index.styles.js
+++ ui/src/components/TopBar/index.styles.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom'
 import { Navigation, Search } from '@commonground/design-system'
 
 export default styled.header`
-    padding: 29px 40px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+  padding: 29px 40px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 `
 export const StyledTitle = styled(Link)`
-    color: ${p => p.theme.color.primary.normal};
-    text-decoration: none;
-    line-height: 42px;
-    font-weight: ${p => p.theme.font.weight.bold};
-    flex: 0 0 auto;
+  color: ${(p) => p.theme.color.primary.normal};
+  text-decoration: none;
+  line-height: 42px;
+  font-weight: ${(p) => p.theme.font.weight.bold};
+  flex: 0 0 auto;
 `
 
 export const StyledNavigation = styled(Navigation)`
-    flex: 0 0 auto;
-    
-    &:last-of-type {
-      margin-right: unset;
-    }
+  flex: 0 0 auto;
+
+  &:last-of-type {
+    margin-right: unset;
+  }
 `
 
 export const StyledForm = styled.form`
