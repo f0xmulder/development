f8ff4b5a13c866ae2668699418a8a7ffeb2bc83b
Bart Jeukendrup <bart@jeukendrup.nl>
2019-06-13 14:11:24 +0200

    fix(ui): do not display underlying elements in mobile menu #97

--- ui/src/components/MobileNavigation/index.styles.js
+++ ui/src/components/MobileNavigation/index.styles.js
@@ -4,17 +4,18 @@ import { NavLink } from 'react-router-dom'
 
 export const NavButton = styled(IconButton)`
   position: fixed;
+  z-index: 2;
   bottom: 20px;
   right: 24px;
   width: 56px;
   height: 56px;
   background-color: white;
-  z-index: 1;
   box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.04), 0 1px 8px rgba(0, 0, 0, 0.04);
 `
 
 export const Navigation = styled.nav`
   position: fixed;
+  z-index: 1;
   top: 0;
   bottom: 0;
   left: 0;
