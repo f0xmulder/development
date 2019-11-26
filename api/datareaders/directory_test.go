2a51be4a84b541378819b31754149fedf19b6b08
Piet Groot Kormelink <piet@mind4simple.nl>
2019-11-26 14:14:45 +0000

    fix(api): Update directory_test.go expected with field names

--- api/datareaders/directory_test.go
+++ api/datareaders/directory_test.go
@@ -11,33 +11,33 @@ import (
 func TestDirectory(t *testing.T) {
 	expected := []models.API{
 		{
-			"company-service",
-			"Test Description",
-			"Test Organization Name",
-			"Test Service Name",
-			"Test API URL",
-			"Test API Type",
-			"Test Specification URL",
-			"Test Documentation URL",
-			[]models.Tag{"test tag"},
-			[]string{"Gouden API"},
-			models.APIContactDetails{
+			ID: "company-service",
+			Description: "Test Description",
+			OrganizationName: "Test Organization Name",
+			ServiceName: "Test Service Name",
+			APIURL: "Test API URL",
+			APIType: "Test API Type",
+			SpecificationURL: "Test Specification URL",
+			DocumentationURL: "Test Documentation URL",
+			Tags: []models.Tag{"test tag"},
+			Badges: []string{"Gouden API"},
+			Contact: models.APIContactDetails{
 				Email: "name@example.nl",
 				Phone: "0031612345678",
 				Fax:   "0031687654321",
 				Chat:  "https://nl-x.slack.com",
 			},
-			false,
-			map[string][]string{
+			IsReferenceImplementation: false,
+			Relations: map[string][]string{
 				"api-id": {"reference-implementation"},
 			},
-			models.APITermsOfUse{
+			TermsOfUse: models.APITermsOfUse{
 				GovernmentOnly:      true,
 				PayPerUse:           false,
 				UptimeGuarantee:     99.9,
 				SupportResponseTime: "2 days",
 			},
-			nil,
+			Scores: nil,
 		},
 	}
 
