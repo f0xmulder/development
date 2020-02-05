fec510b354abeb3209028df4e667c0ccc83c997c
R Lodder <r.lodder@livingit.nl>
2020-02-05 11:36:21 +0000

    feat(ui): #177 Add code splitting to the frontend, so that the specification page with Redoc is only downloaded when the user visits the page

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import MediaQuery from 'react-responsive'
 import { ThemeProvider } from 'styled-components'
@@ -8,11 +8,16 @@ import Home from './pages/Home/Home'
 import SubmitAPI from './pages/SubmitAPI/SubmitAPI'
 import About from './pages/About/About'
 import APIDetail from './pages/APIDetail/APIDetail'
-import APISpecification from './pages/APISpecification/APISpecification'
 import Overview from './pages/Overview/Overview'
 import TopBarContainer from './components/TopBarContainer/TopBarContainer'
 import MobileNavigation from './components/MobileNavigation/MobileNavigation'
 import GlobalStyles from './components/GlobalStyles/GlobalStyles'
+
+const APISpecification = lazy(() =>
+  import(
+    /* webpackChunkName: "api-specification" */ './pages/APISpecification/APISpecification'
+  ),
+)
 
 const LANDSCAPE_PHONES = 576
 
@@ -27,18 +32,19 @@ const App = () => (
         <MediaQuery minWidth={LANDSCAPE_PHONES}>
           <TopBarContainer />
         </MediaQuery>
-
-        <main role="main">
-          <Route path="/" exact component={Home} />
-          <Route path="/detail/:id" exact component={APIDetail} />
-          <Route
-            path="/detail/:id/:environment/specificatie"
-            component={APISpecification}
-          />
-          <Route path="/overzicht" component={Overview} />
-          <Route path="/api-toevoegen" component={SubmitAPI} />
-          <Route path="/over" component={About} />
-        </main>
+        <Suspense fallback={<div />}>
+          <main role="main">
+            <Route path="/" exact component={Home} />
+            <Route path="/detail/:id" exact component={APIDetail} />
+            <Route
+              path="/detail/:id/:environment/specificatie"
+              component={APISpecification}
+            />
+            <Route path="/overzicht" component={Overview} />
+            <Route path="/api-toevoegen" component={SubmitAPI} />
+            <Route path="/over" component={About} />
+          </main>
+        </Suspense>
       </Router>
     </div>
   </ThemeProvider>
