12d39d6a9b5d714bbd498e81c6bd90a5158357c5
Tim de Jong <tim.de.jong@milvum.com>
2020-02-19 11:18:02 +0100

    build(api): Change docker, helm and CI config to use the new Django API

---
+++ api/docker/start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+# Collect static
+python /app/manage.py collectstatic --noinput
+
+# Run migrations
+python /app/manage.py migrate
+
+# Sync API JSONs to the database
+python /app/manage.py sync_apis --api-dir /app/data/apis --fixtures-dir /app/core/fixtures
+
+# Start Gunicorn processes
+uwsgi --http :8000 --module api.wsgi --processes 4 --threads 2 --static-map /admin/static=/app/static
