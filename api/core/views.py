47d52d10a05a0202e611f712a645ea160790b56a
Tim de Jong <tim.de.jong@milvum.com>
2020-02-24 16:09:36 +0100

    fix(api): Make API resource read-only

--- api/core/views.py
+++ api/core/views.py
@@ -1,7 +1,7 @@
 from core.models import API
 from core.serializers import APISerializer
 from django.shortcuts import HttpResponse
-from rest_framework import viewsets
+from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
 # Create your views here.
@@ -9,7 +9,7 @@ def index(request):
     return HttpResponse('WIP Core Index')
 
 
-class APIViewSet(viewsets.ModelViewSet):
+class APIViewSet(ReadOnlyModelViewSet):
     queryset = API.objects.all().order_by('api_id')
     serializer_class = APISerializer
     lookup_field = 'api_id'
