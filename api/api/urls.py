bce82e849b5b014af50477eff5edf23b36ce7800
Jan Kanis <jan.code@jankanis.nl>
2020-06-22 11:25:06 +0200

    feat(api): don't require trailing slash on /admin

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    path('admin', RedirectView.as_view(url='admin/', permanent=True))
 ]
