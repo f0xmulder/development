75035ce33b6f710f14332c6e83a5f20a93302268
Tim de Jong <tim.de.jong@milvum.com>
2020-11-26 15:46:46 +0100

    use real design score data in UI instead of mocks

--- ui/src/models/api.js
+++ ui/src/models/api.js
@@ -38,68 +38,22 @@ const mapTermsOfUse = (termsOfUse) => {
   }
 }
 
-const mockDesignRuleScores = () => {
-  return {
-    results: [
-      {
-        name: 'API-03: Only apply default HTTP operations',
-        description:
-          'A RESTful API is an application programming interface that supports the default HTTP operations GET, PUT, POST, PATCH and DELETE',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-03-only-apply-default-http-operations',
-        success: true,
-        errors: [],
-      },
-      {
-        name: 'API-09: Implement custom representation if supported',
-        description:
-          'Provide a comma-separated list of field names using the query parameter fields te retrieve a custom representation. In case non-existent field names are passed, a 400 Bad Request error message is returned.',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-09-implement-custom-representation-if-supported',
-        success: true,
-        errors: [],
-      },
-      {
-        name: 'API-16: Use OAS 3.0 for documentation',
-        description:
-          'Publish specifications (documentation) as Open API Specification (OAS) 3.0 or higher.',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-16-use-oas-3-0-for-documentation',
-        success: false,
-        errors: ['There is no openapi version found'],
-      },
-      {
-        name: 'API-20: Include the major version number only in the URI',
-        description:
-          'The URI of an API should include the major version number only. The minor and patch version numbers are in the response header of the message. Minor and patch versions have no impact on existing code, but major version do.',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-20-include-the-major-version-number-only-in-ihe-uri',
-        success: true,
-        errors: [],
-      },
-      {
-        name: 'API-48: Leave off trailing slashes from API endpoints',
-        description:
-          "URIs to retrieve collections of resources or individual resources don't include a trailing slash. A resource is only available at one endpoint/path. Resource paths end without a slash.",
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-48-leave-off-trailing-slashes-from-api-endpoints',
-        success: false,
-        errors: [
-          'Path: /provider-latest-badge-1/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-2/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-3/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-4/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-5/{uuid}/ ends with a slash',
-        ],
-      },
-    ],
-  }
-}
+const mapDesignRuleScores = (designRuleScores) => ({
+  results: designRuleScores.results.map((result) => ({
+    name: result.rule_type_name,
+    description: result.rule_type_description,
+    url: result.rule_type_url,
+    success: result.success,
+    errors: result.errors || [],
+  })),
+})
 
 export const modelFromAPIResponse = (api) => {
   const apiType = APIType.valueOf(api.api_type)
   const scores = mapScores(api.scores)
-  const designRuleScores = mockDesignRuleScores()
+  const designRuleScores = api.design_rule_scores
+    ? mapDesignRuleScores(api.design_rule_scores)
+    : null
 
   const totalScore = {
     points: apiType.isREST()
--- ui/src/models/api.mock.js
+++ ui/src/models/api.mock.js
@@ -44,17 +44,18 @@ export const backendApiMock = {
   design_rule_scores: {
     results: [
       {
-        name: 'API-16: Use OAS 3.0 for documentation',
-        description: 'Use Open API Specification (OAS) 3.0 or higher.',
-        url:
+        rule_type_name: 'API-16: Use OAS 3.0 for documentation',
+        rule_type_description:
+          'Use Open API Specification (OAS) 3.0 or higher.',
+        rule_type_url:
           'https://docs.geostandaarden.nl/api/API-Designrules/#api-16-use-oas-3-0-for-documentation',
         success: true,
-        errors: [],
+        errors: null,
       },
       {
-        name: 'API-48: Leave off trailing slashes from API endpoints',
-        description: "URIs don't include a trailing slash.",
-        url:
+        rule_type_name: 'API-48: Leave off trailing slashes from API endpoints',
+        rule_type_description: "URIs don't include a trailing slash.",
+        rule_type_url:
           'https://docs.geostandaarden.nl/api/API-Designrules/#api-48-leave-off-trailing-slashes-from-api-endpoints',
         success: false,
         errors: [
@@ -103,20 +104,21 @@ export const apiMock = {
     hasContactDetails: false,
     providesSla: false,
   },
-  /* TODO re-add this
   designRuleScores: {
     results: [
       {
         name: 'API-16: Use OAS 3.0 for documentation',
         description: 'Use Open API Specification (OAS) 3.0 or higher.',
-        url: 'https://docs.geostandaarden.nl/api/API-Designrules/#api-16-use-oas-3-0-for-documentation',
+        url:
+          'https://docs.geostandaarden.nl/api/API-Designrules/#api-16-use-oas-3-0-for-documentation',
         success: true,
         errors: [],
       },
       {
         name: 'API-48: Leave off trailing slashes from API endpoints',
         description: "URIs don't include a trailing slash.",
-        url: 'https://docs.geostandaarden.nl/api/API-Designrules/#api-48-leave-off-trailing-slashes-from-api-endpoints',
+        url:
+          'https://docs.geostandaarden.nl/api/API-Designrules/#api-48-leave-off-trailing-slashes-from-api-endpoints',
         success: false,
         errors: [
           'Path: /provider-latest-badge-1/{uuid}/ ends with a slash',
@@ -128,65 +130,5 @@ export const apiMock = {
   totalScore: {
     points: 1,
     maxPoints: 2,
-  },
-  */
-  designRuleScores: {
-    results: [
-      {
-        name: 'API-03: Only apply default HTTP operations',
-        description:
-          'A RESTful API is an application programming interface that supports the default HTTP operations GET, PUT, POST, PATCH and DELETE',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-03-only-apply-default-http-operations',
-        success: true,
-        errors: [],
-      },
-      {
-        name: 'API-09: Implement custom representation if supported',
-        description:
-          'Provide a comma-separated list of field names using the query parameter fields te retrieve a custom representation. In case non-existent field names are passed, a 400 Bad Request error message is returned.',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-09-implement-custom-representation-if-supported',
-        success: true,
-        errors: [],
-      },
-      {
-        name: 'API-16: Use OAS 3.0 for documentation',
-        description:
-          'Publish specifications (documentation) as Open API Specification (OAS) 3.0 or higher.',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-16-use-oas-3-0-for-documentation',
-        success: false,
-        errors: ['There is no openapi version found'],
-      },
-      {
-        name: 'API-20: Include the major version number only in the URI',
-        description:
-          'The URI of an API should include the major version number only. The minor and patch version numbers are in the response header of the message. Minor and patch versions have no impact on existing code, but major version do.',
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-20-include-the-major-version-number-only-in-ihe-uri',
-        success: true,
-        errors: [],
-      },
-      {
-        name: 'API-48: Leave off trailing slashes from API endpoints',
-        description:
-          "URIs to retrieve collections of resources or individual resources don't include a trailing slash. A resource is only available at one endpoint/path. Resource paths end without a slash.",
-        url:
-          'https://docs.geostandaarden.nl/api/API-Designrules/#api-48-leave-off-trailing-slashes-from-api-endpoints',
-        success: false,
-        errors: [
-          'Path: /provider-latest-badge-1/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-2/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-3/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-4/{uuid}/ ends with a slash',
-          'Path: /provider-latest-badge-5/{uuid}/ ends with a slash',
-        ],
-      },
-    ],
-  },
-  totalScore: {
-    points: 3,
-    maxPoints: 5,
   },
 }
