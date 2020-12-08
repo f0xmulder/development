aab845821e53c82110c72e5e6ae1120d7ec252f1
Tim de Jong <tim.de.jong@milvum.com>
2020-12-08 15:22:54 +0100

    test(e2e): #307 test score drawer for REST and non-REST API

--- e2e-tests/pages/APIDetail.test.js
+++ e2e-tests/pages/APIDetail.test.js
@@ -35,31 +35,121 @@ describe('API Detail', () => {
             const accessibilityReport = await analyzeAccessibility(page, `api-detail.accessibility.png`)
             expect(accessibilityReport).toHaveNoAccessibilityIssues();
         })
+    })
+})
 
-        describe('opening the score details drawer', () => {
-            beforeAll(async () => {
-                await page.waitForSelector('[data-testid="score-detail-link-desktop"]')
+const REST_TYPES = [
+    'rest_json',
+    'rest_xml',
+]
+const NON_REST_TYPES = [
+    'unknown',
+    'soap_xml',
+    'grpc',
+    'graphql',
+    'sparql',
+    'wfs',
+    'wms',
+]
 
-                await Promise.all([
-                    page.waitForNavigation(),
-                    page.click('[data-testid="score-detail-link-desktop"]')
-                ]);
+describe('API Detail for REST API', () => {
+    beforeAll(async () => {
+        // Search for REST API's
+        const queryString = REST_TYPES.map((type) => `type=${type}`).join('&')
 
-                // Wait for drawer opening animation
-                await page.waitFor(2000)
-            })
+        const baseUrl = getBaseUrl(isDebugging());
+        await page.setBypassCSP(true);
+        await page.goto(`${baseUrl}/apis?${queryString}`, { waitUntil: 'load' });
 
-            it('should show the page title', async () => {
-                // Grab the h1 inside the drawer
-                const html = await page.$eval('[data-testid="content"] h1', e => e.innerHTML)
-                await page.screenshot({ path: 'screenshots/api-score-detail.page-title.png' });
-                expect(html).toBe('Opbouw API Score')
-            })
+        // Navigate to the first API Detail page
+        await page.setViewport({
+            width: 1024,
+            height: 768,
+            deviceScaleFactor: 1,
+        });
 
-            it('should not have accessibility issues', async () => {
-                const accessibilityReport = await analyzeAccessibility(page, `api-score-detail.accessibility.png`)
-                expect(accessibilityReport).toHaveNoAccessibilityIssues();
-            })
+        await page.waitForSelector('[data-test="link"]')
+
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click('[data-test="link"]')
+        ]);
+    })
+
+    describe('opening the Design Rules score drawer', () => {
+        beforeAll(async () => {
+            await page.waitForSelector('[data-testid="score-detail-link-desktop"]')
+
+            await Promise.all([
+                page.waitForNavigation(),
+                page.click('[data-testid="score-detail-link-desktop"]')
+            ]);
+
+            // Wait for drawer opening animation
+            await page.waitFor(2000)
+        })
+
+        it('should show the page title', async () => {
+            // Grab the h1 inside the drawer
+            const html = await page.$eval('[data-testid="content"] h1', e => e.innerHTML)
+            await page.screenshot({ path: 'screenshots/design-rules-score-detail.page-title.png' });
+            expect(html).toBe('Opbouw API Score')
+        })
+
+        it('should not have accessibility issues', async () => {
+            const accessibilityReport = await analyzeAccessibility(page, `design-rules-score-detail.accessibility.png`)
+            expect(accessibilityReport).toHaveNoAccessibilityIssues();
+        })
+    })
+})
+
+describe('API Detail for non-REST API', () => {
+    beforeAll(async () => {
+        // Search for non-REST API's
+        const queryString = NON_REST_TYPES.map((type) => `type=${type}`).join('&')
+
+        const baseUrl = getBaseUrl(isDebugging());
+        await page.setBypassCSP(true);
+        await page.goto(`${baseUrl}/apis?${queryString}`, { waitUntil: 'load' });
+
+        // Navigate to the first API Detail page
+        await page.setViewport({
+            width: 1024,
+            height: 768,
+            deviceScaleFactor: 1,
+        });
+
+        await page.waitForSelector('[data-test="link"]')
+
+        await Promise.all([
+            page.waitForNavigation(),
+            page.click('[data-test="link"]')
+        ]);
+    })
+
+    describe('opening the API score drawer', () => {
+        beforeAll(async () => {
+            await page.waitForSelector('[data-testid="score-detail-link-desktop"]')
+
+            await Promise.all([
+                page.waitForNavigation(),
+                page.click('[data-testid="score-detail-link-desktop"]')
+            ]);
+
+            // Wait for drawer opening animation
+            await page.waitFor(2000)
+        })
+
+        it('should show the page title', async () => {
+            // Grab the h1 inside the drawer
+            const html = await page.$eval('[data-testid="content"] h1', e => e.innerHTML)
+            await page.screenshot({ path: 'screenshots/api-score-detail.page-title.png' });
+            expect(html).toBe('Opbouw API Score')
+        })
+
+        it('should not have accessibility issues', async () => {
+            const accessibilityReport = await analyzeAccessibility(page, `api-score-detail.accessibility.png`)
+            expect(accessibilityReport).toHaveNoAccessibilityIssues();
         })
     })
 })
