c12f48416ecea8d946140b07657ab16c10d07101
Ramsy de Vos <ramsydevos@saxum.nl>
2021-02-12 13:29:55 +0100

    style: cleanup Card styles

--- ui/src/components/Card/Card.styles.js
+++ ui/src/components/Card/Card.styles.js
@@ -7,8 +7,6 @@ import mq from '../../theme/mediaQueries'
 
 export const StyledCard = styled.div`
   width: auto;
-  /* margin: ${(p) => p.theme.tokens.spacing07} -${(p) =>
-    p.theme.tokens.spacing05}; */
   border-radius: 4px;
   background-color: ${(p) => p.theme.tokens.colorBackground};
   box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.24);
