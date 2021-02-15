8d9ebe1c193e58dae4c61b303da856d354dd42c6
Ramsy de Vos <ramsydevos@saxum.nl>
2021-02-15 09:53:04 +0100

    fix: add url and api validation

--- ui/src/components/SubmitCodeForm/validationSchema.js
+++ ui/src/components/SubmitCodeForm/validationSchema.js
@@ -3,11 +3,39 @@
 //
 import * as Yup from 'yup'
 
+Yup.addMethod(Yup.string, 'githubOrGitlabOnly', function (errorMessage) {
+  return this.test(`test-github-gitlab`, errorMessage, function (value) {
+    const { path, createError } = this
+
+    return (
+      (value && value.includes('github')) ||
+      (value && value.includes('gitlab')) ||
+      createError({ path, message: errorMessage })
+    )
+  })
+})
+
+Yup.addMethod(Yup.array, 'usedApi', function (errorMessage) {
+  return this.test(`test-used-api`, errorMessage, function (value) {
+    const { path, createError } = this
+
+    return (
+      (value && value.length) || createError({ path, message: errorMessage })
+    )
+  })
+})
+
 const validationSchema = Yup.object().shape({
-  url: Yup.string().url().required().label('URL naar code'),
-  relatedApis: Yup.array()
-    .transform((value) => value || [])
+  url: Yup.string()
+    .url()
     .required()
+    .githubOrGitlabOnly(
+      'URL naar code moet een GitLab of GitHub repository zijn',
+    )
+    .label('URL naar code'),
+  relatedApis: Yup.array()
+    .required()
+    .usedApi('Selecteer ten minste één gebruikte API')
     .label("Gerelateerde API('s)"),
 })
 
