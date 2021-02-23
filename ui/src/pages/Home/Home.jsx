ad118b66fef2e0a6879497b59b321e7aa49ab639
Ramsy de Vos <ramsydevos@saxum.nl>
2021-02-23 11:57:34 +0100

    fix: #345 remove erronous char when no events

--- ui/src/pages/Home/Home.jsx
+++ ui/src/pages/Home/Home.jsx
@@ -92,7 +92,7 @@ const Home = () => {
         </StyledCard>
       </Links>
 
-      {data?.events?.length && (
+      {!!data?.events?.length && (
         <>
           <H2>Aankomende events</H2>
           <Events>
