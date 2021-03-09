73babd39a5d180f6027686c82f8ccc8963d714a7
Ramsy de Vos <ramsydevos@saxum.nl>
2021-03-09 09:54:34 +0100

    feat: #357 implement react-select-async

--- ui/src/components/SubmitAPIForm/SelectAsync.jsx
+++ ui/src/components/SubmitAPIForm/SelectAsync.jsx
@@ -1,66 +1,94 @@
 // Copyright © VNG Realisatie 2020
 // Licensed under the EUPL
 //
-
-import React from 'react'
+import React, { useState } from 'react'
 import { object } from 'prop-types'
-import { Spinner } from '@commonground/design-system'
-import { StyledReactSelectAsync } from './SubmitAPIForm.styles'
+import { Spinner, ErrorMessage } from '@commonground/design-system'
+import CorRepository from '../../domain/cor-repository'
+import {
+  StyledLink,
+  StyledMessage,
+  StyledReactSelectAsync,
+} from './SelectAsync.styles'
 
-const mapResult = (data) => {
-  return data.map((organisatie) => ({
-    label: organisatie.naam,
-    value: organisatie.oin,
-  }))
-}
+const SelectAsync = ({ field, form: { setFieldValue }, ...props }) => {
+  const [hasError, setError] = useState(false)
 
-const loadOptions = async (search, _, { page }) => {
-  const result = await fetch(
-    `/api/cor/?q=${search}&status=Actief&fields=naam,oin&page=${page}`,
-  )
-  const data = await result.json()
-
-  const options = mapResult(data.results)
-
-  const hasMore = Math.ceil(data.totalResults / data.rowsPerPage) > data.page
-
-  return {
-    options,
-    hasMore,
-    additional: {
-      page: page + 1,
-    },
+  const mapResult = (data) => {
+    return data.map((organisatie) => ({
+      label: organisatie.naam,
+      value: organisatie.oin,
+    }))
   }
-}
 
-const LoadingIndicator = () => {
-  return <Spinner />
-}
+  const loadOptions = async (search, _, { page }) => {
+    setError(false)
+    try {
+      const data = await CorRepository.search(search, page)
+      const hasMore =
+        Math.ceil(data.totalResults / data.rowsPerPage) > data.page
 
-const SelectAsync = ({ field, form: { setFieldValue, ...form }, ...props }) => {
+      const options = mapResult(data.results)
+
+      return {
+        options,
+        hasMore,
+        additional: {
+          page: page + 1,
+        },
+      }
+    } catch (err) {
+      console.error('error', err)
+      setError(true)
+      return {
+        options: [],
+        hasMore: false,
+      }
+    }
+  }
+
+  const LoadingIndicator = () => {
+    return <Spinner />
+  }
+
   return (
-    <StyledReactSelectAsync
-      {...field}
-      {...props}
-      classNamePrefix="ReactSelect"
-      cacheOptions
-      loadOptions={loadOptions}
-      loadingMessage={() => 'Organisaties aan het ophalen ...'}
-      noOptionsMessage={() => 'Niets gevonden!'}
-      defaultOptions
-      label={field.value && field.value.label}
-      value={field.value && field.value.value}
-      onChange={(option) => {
-        setFieldValue(field.name, option.label)
-        setFieldValue('organizationOin', option.value)
-      }}
-      size="l"
-      debounceTimeout={300}
-      components={{ LoadingIndicator }}
-      additional={{
-        page: 1,
-      }}
-    />
+    <>
+      <StyledReactSelectAsync
+        {...field}
+        {...props}
+        classNamePrefix="ReactSelect"
+        cacheOptions
+        loadOptions={loadOptions}
+        loadingMessage={() => (
+          <StyledMessage>Organisaties aan het ophalen ...</StyledMessage>
+        )}
+        noOptionsMessage={() => (
+          <StyledMessage>
+            {hasError ? 'Kan organisaties niet ophalen.' : 'Niets gevonden!'}
+          </StyledMessage>
+        )}
+        defaultOptions
+        label={field.value && field.value.label}
+        value={field.value && field.value.value}
+        onChange={(option) => {
+          setFieldValue(field.name, option.label)
+          setFieldValue('organizationOin', option.value)
+        }}
+        size="l"
+        debounceTimeout={300}
+        components={{ LoadingIndicator }}
+        additional={{
+          page: 1,
+        }}
+      />
+      {hasError && (
+        <ErrorMessage>
+          Kan organisaties niet ophalen. <br />
+          Probeer het later nog eens of voeg de API{' '}
+          <StyledLink to="/apis/add/merge-request">via een MR toe</StyledLink>.
+        </ErrorMessage>
+      )}
+    </>
   )
 }
 
