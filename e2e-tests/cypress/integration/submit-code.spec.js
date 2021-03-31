71be6c2a175b40ceca005023b96c5864166b339f
Ramsy de Vos <ramsydevos@saxum.nl>
2021-03-31 10:54:06 +0200

    fix: #398 accept multiple for submit code

--- e2e-tests/cypress/integration/submit-code.spec.js
+++ e2e-tests/cypress/integration/submit-code.spec.js
@@ -21,7 +21,7 @@ describe('Submit Code', () => {
     cy.get('@url').parent().screenshot()
     cy.get('@url').parent().toMatchImageSnapshot();
     cy.get('button').contains('Project toevoegen').click()
-    cy.contains("de URL is eerder toegevoegd").screenshot()
+    cy.contains(/de URL is eerder toegevoegd|De code is toegevoegd/g).screenshot()
   })
 
   context('a11y', () => {
